3359759085708ce097a06b4ba61ee1e9ae213641
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2024-12-07 13:41:00 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,36 +1,55 @@
 import re
+
 #part 1
 with open("input3.txt") as f:
     lines = f.readlines()
+
+# use regex to find all instances of mul()
 pattern = r"mul\((\d+),(\d+)\)"
-total = 0
 matches = []
 for line in lines:
     matches += re.findall(pattern,line)
+
+# sum the results of each mul()
+total = 0
 for match in matches:
     total += int(match[0])*int(match[1])
-print(total)
+print("Part One: {}".format(total))
 
 #part 2
 with open("input3.txt") as f:
     line = f.read()
-total = 0
-matches = []
 
+# given a string line, save everything before the first dont()
 def removeDont(line):
     find = line.find("don't()")
     if (find == -1):
+        
+        # if no dont(), return everything
         return line
     else:
+        
+        # return everything before dont(), recurse into searching for the next do()
         return line[:find] + addDo(line[find:])
+
+# given a string line, delete everything before the first do()
 def addDo(line):
     find = line.find("do()")
     if (find == -1):
+
+        # if no do(), return nothing
         return ""
     else:
+
+        # return everything after do(), recurse into searching for the next dont()
         return removeDont(line[find:])
 
+# use regex to find all instances of mul() AFTER removing everything inside a dont()
+matches = []
 matches += re.findall(pattern,removeDont(line))
+
+# sum the results of each mul()
+total = 0
 for match in matches:
     total += int(match[0])*int(match[1])
-print(total)
+print("Part Two: {}".format(total))
